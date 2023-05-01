fc038665541ec427c92e0589a95461e9ba62a037
BlfoorTurk <coraxlicorax@gmail.com>
2023-05-01 23:31:39 +0300

    JSDoc added

--- main.js
+++ main.js
@@ -10,6 +10,11 @@ const genID = function (length) {
 };
 
 export class RegexTree {
+  /**
+   * @description A class to test multiple regexes at once.
+   * @param {Array} regexes An array of object with a "regex" property.
+   * @returns {RegexTree}
+   */
   constructor(regexes) {
     if (!Array.isArray(regexes))
       throw new Error("Regexes must be in an array.");
@@ -21,6 +26,10 @@ export class RegexTree {
     });
   }
 
+  /**
+   * @description A function for adding a new regex to the tree.
+   * @param {Object} regex An object with a "regex" property.
+   */
   addRegex(regex) {
     if (!regex.regex)
       throw new Error('The input must have a "regex" property.');
@@ -28,12 +37,21 @@ export class RegexTree {
     this.elements.push(regex);
   }
 
+  /**
+   * @description A function for removing a regex from the tree.
+   * @param {String} id The id of the regex which you want to remove.
+   */
   removeRegex(id) {
     if (!this.elements.some((element) => element.id === id))
       throw new Error("There is no element with this id.");
     this.elements = this.elements.filter((element) => element.id !== id);
   }
 
+  /**
+   * @description A function for testing all of the regexes in the tree for a value.
+   * @param {String} testValue The value which will be tested for all of the regexes in the tree.
+   * @returns {Array} An array of objects with the regexes that have a result property.
+   */
   test(testValue) {
     return this.elements.map((el) => {
       el.result = el.regex.test(testValue);
@@ -41,10 +59,20 @@ export class RegexTree {
     });
   }
 
+  /**
+   * @description A function for testing all of the regexes in the tree for a test value and returning the valid ones.
+   * @param {String} testValue The value which will be tested for all of the regexes in the tree.
+   * @returns {Array} An array of regex objects that are valid for this test value.
+   */
   valids(testValue) {
     return this.elements.filter((element) => element.regex.test(testValue));
   }
 
+  /**
+   * @description A function for testing all of the regexes in the tree for a test value and returning the invalid ones.
+   * @param {String} testValue The value which will be tested for all of the regexes in the tree.
+   * @returns {Array} An array of regex object that are invalid for this test value.
+   */
   invalids(testValue) {
     return this.elements.filter((element) => !element.regex.test(testValue));
   }
